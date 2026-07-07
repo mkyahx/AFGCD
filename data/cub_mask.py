af717dabb720da8f4385d5f6d90b8fcbc375eccb
mkyahx <mkyahx@connect.hku.hk>
2026-07-07 12:34:35 +0800

    feat: add dtheta-guided TIME cross attention

---
+++ data/cub_mask.py
@@ -0,0 +1,257 @@
+import os
+import pandas as pd
+import numpy as np
+import torch
+from copy import deepcopy
+
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.utils import download_url
+from torch.utils.data import Dataset
+
+from data.data_utils import subsample_instances
+from config import cub_root
+
+
+class CustomCub2011Mask(Dataset):
+    base_folder = 'CUB_200_2011/images'
+    url = 'http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz'
+    filename = 'CUB_200_2011.tgz'
+    tgz_md5 = '97eceeb196236b17998738112f37df78'
+
+    def __init__(
+        self,
+        root,
+        mask_root=None,
+        train=True,
+        transform=None,
+        target_transform=None,
+        loader=default_loader,
+        download=True,
+    ):
+
+        self.root = os.path.expanduser(root)
+        self.mask_root = os.path.expanduser(mask_root) if mask_root is not None else None
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.loader = loader
+        self.train = train
+
+
+        if download:
+            self._download()
+
+        if not self._check_integrity():
+            raise RuntimeError('Dataset not found or corrupted.' +
+                               ' You can use download=True to download it')
+
+        self.uq_idxs = np.array(range(len(self)))
+
+    def _load_metadata(self):
+        images = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'images.txt'), sep=' ',
+                             names=['img_id', 'filepath'])
+        image_class_labels = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'image_class_labels.txt'),
+                                         sep=' ', names=['img_id', 'target'])
+        train_test_split = pd.read_csv(os.path.join(self.root, 'CUB_200_2011', 'train_test_split.txt'),
+                                       sep=' ', names=['img_id', 'is_training_img'])
+
+        data = images.merge(image_class_labels, on='img_id')
+        self.data = data.merge(train_test_split, on='img_id')
+
+        if self.train:
+            self.data = self.data[self.data.is_training_img == 1]
+        else:
+            self.data = self.data[self.data.is_training_img == 0]
+
+    def _check_integrity(self):
+        try:
+            self._load_metadata()
+        except Exception:
+            return False
+
+        for index, row in self.data.iterrows():
+            filepath = os.path.join(self.root, self.base_folder, row.filepath)
+            if not os.path.isfile(filepath):
+                print(filepath)
+                return False
+        return True
+
+    def _download(self):
+        import tarfile
+
+        if self._check_integrity():
+            print('Files already downloaded and verified')
+            return
+
+        download_url(self.url, self.root, self.filename, self.tgz_md5)
+
+        with tarfile.open(os.path.join(self.root, self.filename), "r:gz") as tar:
+            tar.extractall(path=self.root)
+
+    def __len__(self):
+        return len(self.data)
+
+    def _get_mask(self, filepath):
+        if self.mask_root is None:
+            return np.ones((14, 14), dtype=np.float32)
+
+        mask_path = os.path.join(self.mask_root, os.path.splitext(filepath)[0] + '.npy')
+        if not os.path.isfile(mask_path):
+            raise FileNotFoundError(f'Mask file not found: {mask_path}')
+        return np.load(mask_path).astype(np.float32)
+
+    def __getitem__(self, idx):
+        sample = self.data.iloc[idx]
+        path = os.path.join(self.root, self.base_folder, sample.filepath)
+        target = sample.target - 1  # Targets start at 1 by default, so shift to 0
+        img = self.loader(path)
+        # Materialize as an owned tensor to avoid DataLoader collate issues with
+        # non-resizable storages created from NumPy-backed arrays.
+        patch_mask = torch.tensor(self._get_mask(sample.filepath).copy(), dtype=torch.float32)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target, self.uq_idxs[idx], patch_mask
+
+
+def subsample_dataset(dataset, idxs):
+
+    mask = np.zeros(len(dataset)).astype('bool')
+    mask[idxs] = True
+
+    dataset.data = dataset.data[mask]
+    dataset.uq_idxs = dataset.uq_idxs[mask]
+
+    return dataset
+
+
+def subsample_classes(dataset, include_classes=range(160)):
+
+    include_classes_cub = np.array(include_classes) + 1     # CUB classes are indexed 1 --> 200 instead of 0 --> 199
+    cls_idxs = [x for x, (_, r) in enumerate(dataset.data.iterrows()) if int(r['target']) in include_classes_cub]
+
+    # TODO: For now have no target transform
+    target_xform_dict = {}
+    for i, k in enumerate(include_classes):
+        target_xform_dict[k] = i
+
+    dataset = subsample_dataset(dataset, cls_idxs)
+
+    dataset.target_transform = lambda x: target_xform_dict[x]
+
+    return dataset
+
+
+def get_train_val_indices(train_dataset, val_split=0.2):
+
+    train_classes = np.unique(train_dataset.data['target'])
+
+    # Get train/test indices
+    train_idxs = []
+    val_idxs = []
+    for cls in train_classes:
+
+        cls_idxs = np.where(train_dataset.data['target'] == cls)[0]
+
+        v_ = np.random.choice(cls_idxs, replace=False, size=((int(val_split * len(cls_idxs))),))
+        t_ = [x for x in cls_idxs if x not in v_]
+
+        train_idxs.extend(t_)
+        val_idxs.extend(v_)
+
+    return train_idxs, val_idxs
+
+
+class MergedDatasetMask(Dataset):
+    def __init__(self, labelled_dataset, unlabelled_dataset):
+        self.labelled_dataset = labelled_dataset
+        self.unlabelled_dataset = unlabelled_dataset
+        self.target_transform = None
+
+    def __getitem__(self, item):
+        if item < len(self.labelled_dataset):
+            img, label, uq_idx, patch_mask = self.labelled_dataset[item]
+            labeled_or_not = 1
+        else:
+            img, label, uq_idx, patch_mask = self.unlabelled_dataset[item - len(self.labelled_dataset)]
+            labeled_or_not = 0
+
+        return img, label, uq_idx, np.array([labeled_or_not]), patch_mask
+
+    def __len__(self):
+        return len(self.unlabelled_dataset) + len(self.labelled_dataset)
+
+
+def get_cub_mask_datasets(train_transform, test_transform, train_classes=range(160), prop_train_labels=0.8,
+                          split_train_val=False, seed=0, download=False, mask_root=None):
+
+    np.random.seed(seed)
+
+    # Init entire training set
+    whole_training_set = CustomCub2011Mask(
+        root=cub_root,
+        mask_root=mask_root,
+        transform=train_transform,
+        train=True,
+        download=download,
+    )
+
+    # Get labelled training set which has subsampled classes, then subsample some indices from that
+    train_dataset_labelled = subsample_classes(deepcopy(whole_training_set), include_classes=train_classes)
+    subsample_indices = subsample_instances(train_dataset_labelled, prop_indices_to_subsample=prop_train_labels)
+    train_dataset_labelled = subsample_dataset(train_dataset_labelled, subsample_indices)
+
+    # Split into training and validation sets
+    train_idxs, val_idxs = get_train_val_indices(train_dataset_labelled)
+    train_dataset_labelled_split = subsample_dataset(deepcopy(train_dataset_labelled), train_idxs)
+    val_dataset_labelled_split = subsample_dataset(deepcopy(train_dataset_labelled), val_idxs)
+    val_dataset_labelled_split.transform = test_transform
+
+    # Get unlabelled data
+    unlabelled_indices = set(whole_training_set.uq_idxs) - set(train_dataset_labelled.uq_idxs)
+    train_dataset_unlabelled = subsample_dataset(deepcopy(whole_training_set), np.array(list(unlabelled_indices)))
+
+    # Get test set for all classes
+    test_dataset = CustomCub2011Mask(
+        root=cub_root,
+        mask_root=mask_root,
+        transform=test_transform,
+        train=False,
+    )
+
+    # Either split train into train and val or use test set as val
+    train_dataset_labelled = train_dataset_labelled_split if split_train_val else train_dataset_labelled
+    val_dataset_labelled = val_dataset_labelled_split if split_train_val else None
+
+    all_datasets = {
+        'train_labelled': train_dataset_labelled,
+        'train_unlabelled': train_dataset_unlabelled,
+        'val': val_dataset_labelled,
+        'test': test_dataset,
+    }
+
+    return all_datasets
+
+if __name__ == '__main__':
+
+    x = get_cub_mask_datasets(None, None, split_train_val=False,
+                              train_classes=range(100), prop_train_labels=0.5)
+
+    print('Printing lens...')
+    for k, v in x.items():
+        if v is not None:
+            print(f'{k}: {len(v)}')
+
+    print('Printing labelled and unlabelled overlap...')
+    print(set.intersection(set(x['train_labelled'].uq_idxs), set(x['train_unlabelled'].uq_idxs)))
+    print('Printing total instances in train...')
+    print(len(set(x['train_labelled'].uq_idxs)) + len(set(x['train_unlabelled'].uq_idxs)))
+
+    print(f'Num Labelled Classes: {len(set(x["train_labelled"].data["target"].values))}')
+    print(f'Num Unabelled Classes: {len(set(x["train_unlabelled"].data["target"].values))}')
+    print(f'Len labelled set: {len(x["train_labelled"])}')
+    print(f'Len unlabelled set: {len(x["train_unlabelled"])}')
---
+++ models/tokenAdaptive_guided.py
@@ -0,0 +1,175 @@
+import torch
+import torch.nn as nn
+
+from models.TIME_guided import Token_Importance_Measurer_Guided
+
+class DINOHead(nn.Module):
+    def __init__(self, in_dim, out_dim, use_bn=False, norm_last_layer=True, 
+                 nlayers=3, hidden_dim=2048, bottleneck_dim=256):
+        super().__init__()
+        nlayers = max(nlayers, 1)
+        if nlayers == 1:
+            self.mlp = nn.Linear(in_dim, bottleneck_dim)
+        elif nlayers != 0:
+            layers = [nn.Linear(in_dim, hidden_dim)]
+            if use_bn:
+                layers.append(nn.BatchNorm1d(hidden_dim))
+            layers.append(nn.GELU())
+            for _ in range(nlayers - 2):
+                layers.append(nn.Linear(hidden_dim, hidden_dim))
+                if use_bn:
+                    layers.append(nn.BatchNorm1d(hidden_dim))
+                layers.append(nn.GELU())
+            layers.append(nn.Linear(hidden_dim, bottleneck_dim))
+            self.mlp = nn.Sequential(*layers)
+        self.apply(self._init_weights)
+        self.last_layer = nn.utils.weight_norm(nn.Linear(in_dim, out_dim, bias=False))
+        self.last_layer.weight_g.data.fill_(1)
+        if norm_last_layer:
+            self.last_layer.weight_g.requires_grad = False
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x, aux_preds = x
+        x_proj = self.mlp(x)
+        x = nn.functional.normalize(x, dim=-1, p=2)
+        
+        logits = self.last_layer(x)
+        return x_proj, logits, aux_preds
+
+class TokenAdaptivePrunerGuided(nn.Module):
+    def __init__(self, args, backbone):
+        super().__init__()
+        
+        self.num_labeled_classes = args.num_labeled_classes
+        self.feat_dim = args.feat_dim
+        self.threshold = args.threshold
+        self.dtheta = args.dtheta
+        self.pretrainModel = backbone
+        if not isinstance(args.image_size, tuple):
+            self.mask_length = int(args.image_size / 16)
+        else:
+            self.mask_length = int(args.image_size[0] / 16)
+        self.batch_size = args.batch_size
+        self.time_num_heads = backbone.blocks[0].attn.num_heads
+        
+        self.norm = nn.LayerNorm(args.feat_dim, eps=1e-6)
+
+        self.init_TIME()
+        self.set_remain_token_num(0)
+
+        self.flip_flag = 0
+    
+    def get_remain_token_num(self):
+        return self.remain_token_num
+    
+    def set_remain_token_num(self, num):
+        self.remain_token_num = num
+
+    def init_TIME(self):
+        self.TIME = nn.ModuleList([   
+                    Token_Importance_Measurer_Guided(
+                        num_classes=self.num_labeled_classes,
+                        feat_dim=self.feat_dim,
+                        num_heads=self.time_num_heads,
+                        dtheta=self.dtheta,
+                    ) for _ in range(len(self.pretrainModel.blocks) - 1)
+                ])
+
+    def forward(self, data):
+        if len(data) == 3:
+            imgs, training, patch_mask = data
+        else:
+            imgs, training = data
+            patch_mask = None
+        preds = []
+        attn_scores = []
+        
+        x = self.pretrainModel.prepare_tokens(imgs)
+        for i, blk in enumerate(self.pretrainModel.blocks):
+            if i < len(self.pretrainModel.blocks) - 1:
+                x = self.block_forward(blk, x)
+                # All Layer TIME
+                attn_score, pred = self.TIME[i](x.detach(), patch_mask=patch_mask)
+                preds.append(pred)
+                attn_scores.append(attn_score)
+            else:
+                last_x = x
+                token_masks = self.prune(attn_scores, training)
+                last_x_with_mask = self.block_forward(blk, last_x, token_masks)
+                last_x_with_mask = self.pretrainModel.norm(last_x_with_mask)
+                # prune token not remain
+                mask_num = token_masks.sum(1).unsqueeze(-1)
+                last_x = (last_x_with_mask * token_masks.unsqueeze(-1).expand_as(last_x_with_mask)).sum(1) / mask_num
+                x = self.norm(last_x)
+                
+        return x, preds
+
+    def prune(self, scores, training):
+
+        patch_scores = torch.cat([score[:, 1:].unsqueeze(1) for score in scores], dim=1)
+        patch_scores = patch_scores.softmax(-1).mean(1)
+ 
+        patch_sort, patch_idx_sort = torch.sort(patch_scores, dim=1, descending=False)
+        patch_cum = torch.cumsum(patch_sort, dim=1)
+        patch_masks = patch_cum > (self.threshold)
+
+        new_masks = torch.zeros_like(patch_masks).bool().cuda()
+        rows, _ = torch.where(patch_masks)
+        selected_idxs = patch_idx_sort[patch_masks]
+        new_masks[rows, selected_idxs] = True
+        patch_masks = new_masks
+
+        # remain one view all Patches
+        if training:
+            patch_masks[self.batch_size:] = True
+
+        remain_token_num = patch_masks[:self.batch_size].sum()
+
+        self.set_remain_token_num(self.get_remain_token_num() + remain_token_num)
+
+        unit_mask = torch.ones(patch_masks.shape[0], 1).bool().cuda()
+        token_masks = torch.cat([unit_mask, patch_masks], dim=1)
+
+        return token_masks
+
+    def block_forward(self, blk, x, token_mask=None, return_attn=False):
+        if token_mask is not None:
+            y, attn = self.attn_forward(blk.attn, blk.norm1(x), token_mask)
+        else:
+            y, attn = blk.attn(blk.norm1(x))
+
+        x = x + blk.drop_path(y)
+        x = x + blk.drop_path(blk.mlp(blk.norm2(x)))
+        
+        if return_attn:
+            return x, attn
+        return x
+
+    def attn_forward(self, attnModel, x, token_mask):
+        B, N, C = x.shape
+        qkv = attnModel.qkv(x).reshape(B, N, 3, attnModel.num_heads, C // attnModel.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        attn = (q @ k.transpose(-2, -1)) * attnModel.scale
+
+        mask = token_mask.unsqueeze(2).float() @ token_mask.unsqueeze(1).float()
+        mask = mask.unsqueeze(1).repeat(1, attn.shape[1], 1, 1).bool()
+        attn = attn.masked_fill(~mask, float('-inf'))
+
+        attn = attn.softmax(dim=-1)
+        attn = attn.masked_fill(~mask, 0.0)
+
+        attn = attnModel.attn_drop(attn)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = attnModel.proj(x)
+        x = attnModel.proj_drop(x)
+        return x, attn
+    
+
+    
